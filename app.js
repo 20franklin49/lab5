07fb98687e791076c79764b5ab3e791c70b393d5
Franklin Li <frl003@ucsd.edu>
2018-02-08 16:32:45 -0800

    complete lab5

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var handlebars = require('express3-handlebars');
 var index = require('./routes/index');
 // Example route
 // var user = require('./routes/user');
-var add = require('./routes/add.js');
+var add = require('./routes/add');
 
 var app = express();
 
@@ -39,7 +39,7 @@ if ('development' == app.get('env')) {
 app.get('/', index.view);
 // Example route
 // app.get('/users', user.list);
-app.get('/', add.addFriend);
+app.get('/add', add.addFriend);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/add.js
+++ routes/add.js
@@ -3,7 +3,12 @@ var data = require("../data.json");
 exports.addFriend = function(req, res) {    
 	req.query.name;
 	req.query.description;
-	var newFriend = {name: req.query.name, description: req.query.description, imageURL:"http://lorempixel.com/400/400/people"};
+	var newFriend = {
+		"name": req.query.name,
+		 "description": req.query.description,
+		  "imageURL": "http://lorempixel.com/400/400/people"
+	};
 	console.log();
 	data.friends.push(newFriend);
+	res.render('index');
  }
